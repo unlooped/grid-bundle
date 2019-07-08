8b782ab5e706cac6431b31f245b9a509c424bac9
Steffen Persch <s.persch@unlooped.com>
2019-07-08 16:34:12 +0200

    [ENH] Add Boolean Column

---
+++ ColumnType/BooleanColumn.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Unlooped\GridBundle\ColumnType;
+
+class BooleanColumn extends AbstractColumnType {
+
+    protected $template = '@UnloopedGrid/column_types/boolean.html.twig';
+
+}
