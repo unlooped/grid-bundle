51da03cfd2c7c5a239967894a943a0da4c6fee2e
Steffen Persch <s.persch@unlooped.com>
2019-08-05 13:19:39 +0200

    [REF] define operators fir TextFilterType

--- FilterType/TextFilterType.php
+++ FilterType/TextFilterType.php
@@ -6,5 +6,18 @@ namespace Unlooped\GridBundle\FilterType;
 
 class TextFilterType extends FilterType
 {
+    public static function getAvailableOperators(): array
+    {
+        return [
+            self::EXPR_CONTAINS     => self::EXPR_CONTAINS,
+            self::EXPR_NOT_CONTAINS => self::EXPR_NOT_CONTAINS,
+            self::EXPR_BEGINS_WITH  => self::EXPR_BEGINS_WITH,
+            self::EXPR_ENDS_WITH    => self::EXPR_ENDS_WITH,
+            self::EXPR_EQ           => self::EXPR_EQ,
+            self::EXPR_NEQ          => self::EXPR_NEQ,
+            self::EXPR_IS_EMPTY     => self::EXPR_IS_EMPTY,
+            self::EXPR_IS_NOT_EMPTY => self::EXPR_IS_NOT_EMPTY,
+        ];
+    }
 
 }
