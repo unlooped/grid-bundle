9ee7bceb5c7588e26ff6704a3bf57b370df8a0f1
Steffen Persch <s.persch@unlooped.com>
2019-08-19 17:10:43 +0200

    [ENH] basic autocomplete type

---
+++ FilterType/AutocompleteFilterType.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace Unlooped\GridBundle\FilterType;
+
+
+use App\Entity\Influencer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Tetranz\Select2EntityBundle\Form\Type\Select2EntityType;
+
+class AutocompleteFilterType extends FilterType
+{
+    public static function getAvailableOperators(): array
+    {
+        return [
+            self::EXPR_EQ           => self::EXPR_EQ,
+            self::EXPR_NEQ          => self::EXPR_NEQ,
+        ];
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults([
+            'route' => '',
+            'entity' => '',
+            'minimum_input_length' => 2,
+        ]);
+
+        $resolver->setRequired('route');
+        $resolver->setRequired('entity');
+        $resolver->setRequired('minimum_input_length');
+
+        $resolver->setAllowedTypes('route', 'string');
+        $resolver->setAllowedTypes('entity', 'string');
+        $resolver->setAllowedTypes('minimum_input_length', 'int');
+    }
+
+
+    public function buildForm($builder, array $options = [], $data = null): void
+    {
+        $builder
+            ->remove('value')
+            ->add('value', Select2EntityType::class, [
+                'multiple' => false,
+                'remote_route' => $this->getOptions()['route'],
+                'class' => $this->getOptions()['entity'],
+                'primary_key' => 'id',
+                'minimum_input_length' => $this->getOptions()['entity'],
+                'page_limit' => 10,
+                'allow_clear' => true,
+                'delay' => 250,
+                'cache' => true,
+                'cache_timeout' => 60000, // if 'cache' is true
+                'language' => 'en',
+            ]);
+    }
+
+}
