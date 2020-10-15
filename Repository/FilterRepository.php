f7d2c4aa3ad4ad875de2cc44a010018beb5746ca
Steffen Persch <s.persch@unlooped.com>
2020-10-15 17:10:45 +0200

    composer update + doctrine fix

--- Repository/FilterRepository.php
+++ Repository/FilterRepository.php
@@ -3,7 +3,7 @@
 namespace Unlooped\GridBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\NonUniqueResultException;
 use Unlooped\GridBundle\Entity\Filter;
 
