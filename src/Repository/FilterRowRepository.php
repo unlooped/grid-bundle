fc456da09afe0850d2c2c97e7969a07fe371b8a3
core23 <mail@core23.de>
2020-11-08 20:38:35 +0100

    Remove dead code

--- src/Repository/FilterRowRepository.php
+++ src/Repository/FilterRowRepository.php
@@ -18,33 +18,4 @@ class FilterRowRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, FilterRow::class);
     }
-
-    // /**
-    //  * @return FilterRow[] Returns an array of FilterRow objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?FilterRow
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
