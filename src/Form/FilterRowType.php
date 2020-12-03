06b952e3a375a41a2af4783d59cf5e9f40cba65b
core23 <mail@core23.de>
2020-12-03 20:44:54 +0100

    Simplify FilterRowType

--- src/Form/FilterRowType.php
+++ src/Form/FilterRowType.php
@@ -2,6 +2,7 @@
 
 namespace Unlooped\GridBundle\Form;
 
+use RuntimeException;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,7 +12,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Unlooped\GridBundle\Entity\FilterRow;
 use Unlooped\GridBundle\Filter\Filter;
 use Unlooped\GridBundle\FilterType\AbstractFilterType;
-use Unlooped\GridBundle\FilterType\FilterType;
 
 class FilterRowType extends AbstractType
 {
@@ -29,7 +29,7 @@ class FilterRowType extends AbstractType
             ->add('value', null, ['required' => false])
         ;
 
-        $builder->addEventListener(FormEvents::POST_SET_DATA, static function (FormEvent $event) use ($options) {
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) use ($options) {
             if (null === $event->getData()) {
                 return;
             }
@@ -38,50 +38,15 @@ class FilterRowType extends AbstractType
             $data = $event->getData();
             $form = $event->getForm();
 
-            /** @var array<string, Filter> $filters */
-            $filters = $options['filters'];
-            $filter  = $filters[$data->getField()];
+            $filter = $this->getFilterFromOptions($options, $data->getField());
 
             if ($data->getField()) {
                 $filterType = $filter->getType();
-
-                $metaData                    = $data->getMetaData();
-                $metaData['_original_field'] = $data->getField();
-
-                $data->setMetaData($metaData);
                 $filterType->postSetFormData($form, $filter->getOptions(), $data, $event);
             }
         });
 
-        $builder->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event) use ($options) {
-            if (null === $event->getData()) {
-                return;
-            }
-
-            /** @var FilterRow $data */
-            $data    = $event->getData();
-            $form    = $event->getForm();
-
-            /** @var array<string, Filter> $filters */
-            $filters = $options['filters'];
-
-            if (\array_key_exists('_original_field', $data->getMetaData()) && $data->getMetaData()['_original_field'] !== $data->getField()) {
-                /** @var FilterType $originalFilterType */
-                $originalFilterType = $filters[$data->getMetaData()['_original_field']];
-                $origFields         = $originalFilterType->getFormFieldNames();
-
-                /** @var FilterType $filterType */
-                $filterType = $filters[$data->getField()]->getType();
-                $newFields  = $filterType->getFormFieldNames();
-
-                $fieldsToRemove = array_diff($origFields, $newFields);
-                foreach ($fieldsToRemove as $fieldToRemove) {
-                    $form->remove($fieldToRemove);
-                }
-            }
-        });
-
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, static function (FormEvent $event) use ($options) {
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use ($options) {
             if (null === $event->getData()) {
                 return;
             }
@@ -89,15 +54,13 @@ class FilterRowType extends AbstractType
             $data    = $event->getData();
             $form    = $event->getForm();
 
-            /** @var array<string, Filter> $filters */
-            $filters = $options['filters'];
-            $filter     = $filters[$data['field']];
+            $filter = $this->getFilterFromOptions($options, $data['field']);
 
             $filterType = $filter->getType();
             $filterType->preSubmitFormData($form, $filter->getOptions(), $data, $event);
         });
 
-        $builder->addEventListener(FormEvents::POST_SUBMIT, static function (FormEvent $event) use ($options) {
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($options) {
             if (null === $event->getData()) {
                 return;
             }
@@ -106,9 +69,7 @@ class FilterRowType extends AbstractType
             $data    = $event->getData();
             $form    = $event->getForm();
 
-            /** @var array<string, Filter> $filters */
-            $filters = $options['filters'];
-            $filter     = $filters[$data->getField()];
+            $filter = $this->getFilterFromOptions($options, $data->getField());
 
             $filterType = $filter->getType();
             $filterType->postFormSubmit($form, $filter->getOptions(), $data, $event);
@@ -124,5 +85,17 @@ class FilterRowType extends AbstractType
         ]);
 
         $resolver->setRequired('fields');
+    }
+
+    private function getFilterFromOptions(array $options, ?string $field): Filter
+    {
+        /** @var array<string, Filter> $filters */
+        $filters = $options['filters'];
+
+        if (null === $field || null === $filters[$field]) {
+            throw new RuntimeException('Type is not defined');
+        }
+
+        return $filters[$field];
     }
 }
