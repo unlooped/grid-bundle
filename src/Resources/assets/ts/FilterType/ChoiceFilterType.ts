08969a9bba4b20a4bb4227db83e32831138628e8
Steffen Persch <s.persch@unlooped.com>
2021-09-06 22:44:40 +0200

    [BUG] use proper options path

--- src/Resources/assets/ts/FilterType/ChoiceFilterType.ts
+++ src/Resources/assets/ts/FilterType/ChoiceFilterType.ts
@@ -5,7 +5,7 @@ export class ChoiceFilterType extends FilterType {
     public updateValueInput(row: Element, fieldName: string, config: any) {
         super.updateValueInput(row, fieldName, config);
 
-        if (config.use_select2) {
+        if (config.options.use_select2) {
             // @ts-ignore
             jQuery('.initSelect2').select2();
         }
